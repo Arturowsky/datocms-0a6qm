7e0e747c0396abbf02d080c8db4e73e496f465d3
Artur <artur.lewandowicz@gmail.com>
2021-08-28 12:57:37 +0200

    implementation of bottom right menu

--- src/components/BottomRightMenu.js
+++ src/components/BottomRightMenu.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../scss/settings.scss"
 import "../scss/btmenu.scss"
 const BottomRightMenu = () => {
-
+    const [disableMenu, setDisableMenu] = useState("block")
+    const turnoff = () => {
+        setDisableMenu("none")
+    }
     return (
         <>
-        <div className="btm-wrapper">
-            <div>1</div>
+        <div className="btm-wrapper" style={{display: disableMenu}}>
+            <div><button onClick={turnoff}>X</button></div>
         </div>
         </>
     )
